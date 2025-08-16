817e70d9f0fd27c8c6378e6247820c43148a0db5
Lazjessicah <166005469+LJ-vdZ@users.noreply.github.com>
2025-08-16 22:00:54 +0200

    UI StartMenu & PauseMenu
    - Start menu completed (need to add tutorial)
    - Pause menu completed
    - busy with death menu

--- GADS6222_GameJam/Assets/Scripts/UIManager.cs
+++ GADS6222_GameJam/Assets/Scripts/UIManager.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void PlayGame() 
     {
-        
+        SceneManager.LoadScene("GameLevell");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitApp() 
     {
-        
+        Application.Quit();
     }
+
+    public void Home()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+
 }
